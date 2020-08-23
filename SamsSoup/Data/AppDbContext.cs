77ec4ee0516f2893742e6257213d7e34bc514ce4
Samuel Ketels <ketels.samuel@gmail.com>
2020-08-23 20:59:48 +0200

    Added some styling and EF Core

---
+++ SamsSoup/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamsSoup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamsSoup.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Soup> Soups { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- SamsSoup/Startup.cs
+++ SamsSoup/Startup.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SamsSoup.Data;
 using SamsSoup.Data.Interfaces;
 using SamsSoup.Data.Repositories;
 
@@ -14,10 +17,19 @@ namespace SamsSoup
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddScoped<ISoupRepository, MockSoupRepository>();
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 
